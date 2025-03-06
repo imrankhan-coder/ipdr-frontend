806f2590cf33640aeaafa7b42652b121f897c879
imrankhan-coder <61090973+imrankhan-coder@users.noreply.github.com>
2025-03-06 19:09:31 +0500

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-64 h-screen bg-gray-800 text-white p-4">
+      <ul>
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
 
