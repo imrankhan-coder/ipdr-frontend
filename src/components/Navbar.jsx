abe82b8c906b370211b2f49fadce27ec10feede5
imrankhan-coder <61090973+imrankhan-coder@users.noreply.github.com>
2025-03-06 19:09:10 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-900 p-4 text-white flex justify-between">
+      <h1 className="text-lg">IPDR Portal</h1>
+      <div>
+        <Link to="/" className="mr-4">Dashboard</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
 
