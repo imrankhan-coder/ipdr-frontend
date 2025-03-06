b2e63b232b0610ffcbd990f970929cb3909b2ff5
imrankhan-coder <61090973+imrankhan-coder@users.noreply.github.com>
2025-03-06 19:08:28 +0500

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,1 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
 
